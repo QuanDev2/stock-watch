5f6da276233cc17f0a4f7ed7fe752629735e376c
unknown <Gonzakad@oregonstate.edu>
2021-03-15 18:44:20 -0700

    Initial code for StockSummaryCard and base code for Homepage

--- src/components/StockSummaryCard.js
+++ src/components/StockSummaryCard.js
@@ -3,10 +3,34 @@ import styled from '@emotion/styled'
 
 const Container = styled.div`
 	border: solid 1px #bbb;
+	box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  	transition: 0.3s;
+	margin:10px;
+	min-width:600px;
+	max-width:800px;
+	
+	&:hover {
+		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+	}
+
+	& > p {
+		padding:5px;
+	}
+
+	& > h2 {
+		padding-left:2px;
+	}
 `
 
-function StockSummaryCard() {
-	return <Container></Container>
+function StockSummaryCard({ currentPrice, openChange, volume }) {
+	return (
+		<Container>
+			<h2>DOW</h2>
+			<p>Current Price: ${currentPrice} </p>
+			<p>Open Change: %{openChange} </p>
+			<p> Volume: {volume} shares </p>
+		</Container>
+	)
 }
 
 export default StockSummaryCard
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import StockSummaryCard from '../../components/StockSummaryCard'
 
-const Container = styled.div``
+const Container = styled.div`
+	
+`
 
-const MarketSummaryContainer = styled.div``
+const MarketSummaryContainer = styled.div`
+	display: flex;
+	flex-direction:column;
+	justify-content: center;
+  	align-items: center;
+`
+	
 
 function Homepage() {
 	return (
 		<Container>
-			<MarketSummaryContainer></MarketSummaryContainer>
+			<MarketSummaryContainer>
+				<StockSummaryCard 
+					currentPrice = {500}
+					openChange = {15}
+					volume = {1000000}
+				/>
+				<StockSummaryCard 
+					currentPrice = {500}
+					openChange = {15}
+					volume = {1000000}
+				/>
+				<StockSummaryCard 
+					currentPrice = {500}
+					openChange = {15}
+					volume = {1000000}
+				/>
+			</MarketSummaryContainer>
 		</Container>
 	)
 }
