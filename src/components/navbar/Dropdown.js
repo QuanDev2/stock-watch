5872d4750b0bf7854ba1e242dbcf9871bf12cb35
quandev2 <qnguyen.dev2+dev+git@gmail.com>
2021-03-16 19:26:50 -0700

    finished dropdown results. Need to implement individual route and onclick event listener for each dropdown item

---
+++ src/components/navbar/Dropdown.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import { BOX_SHADOW } from '../../utils/GlobalStyle'
+import DropdownItem from './DropdownItem'
+
+const Container = styled.div`
+	position: relative;
+	background: white;
+	padding: 0.5rem 0;
+
+	border-radius: 0 0 8px 8px;
+	box-shadow: ${BOX_SHADOW.SHADOW_2};
+`
+
+function Dropdown({ contentList, open, searchInput }) {
+	console.log(`searchInput: ${searchInput}`)
+	console.log(`contentList: ${contentList}`)
+	if (searchInput && contentList.length === 0) {
+		return (
+			<Container>
+				<span
+					style={{
+						padding: '2rem 1rem'
+					}}
+				>
+					We were unable to find any results for your search.
+				</span>
+			</Container>
+		)
+	} else if (!open || !searchInput) {
+		return null
+	} else {
+		return (
+			<Container>
+				<ul>
+					{contentList.map(item => (
+						<DropdownItem
+							itemKey={item.key}
+							symbol={item.symbol}
+							name={item.name}
+						/>
+					))}
+				</ul>
+			</Container>
+		)
+	}
+}
+export default Dropdown
