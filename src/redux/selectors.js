b0692d988b1088a5091fab47f373dc691965765c
Kade <Gonzakad@oregonstate.edu>
2021-03-19 13:31:04 -0700

    Added functionality for  button

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,24 @@
 export function getWatchList(state) {
 	return state
 }
+
+
+export const isInWatchlist = symbol => state => {
+	state.array.forEach(element => {
+		if (element.symbol === symbol) {
+			return true
+		}
+	})
+	return false
+}
+
+export function watchListSymbols(state) {
+	return state.map(stock => {
+		return stock.symbol
+	})
+}
+	
+
+
+	
+
